147518491674819fd4fbd7864997edec38928610
adminsco <geniyoctober@yandex.ru>
2020-10-06 22:10:14 +0800

    bot1

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ que = lines1[que]
 @bot.message_handler(commands=['start'])
 def start(message):
 		bot.send_message(message.from_user.id, str(que))
-		bot.send_message(message.from_user.id, 'Расшифруй ребус')
+		bot.send_message(message.from_user.id, "Расшифруй ребус")
 
 @bot.message_handler(content_types=['text'])
 def start_welcome(message):
@@ -27,11 +27,11 @@ def start_welcome(message):
         bot.send_message(message.from_user.id, "Всё верно, напиши своё имя и фамилию для рейтинга:)")
         bot.register_next_step_handler(message, get_name); #следующий шаг – функция get_name
     else:
-        bot.send_message(message.from_user.id, 'Напиши верный ответ')
+        bot.send_message(message.from_user.id, "Напиши верный ответ")
 
 def get_name(message):
     global name
     name = message.text
-    bot.send_message(message.from_user.id, name + 'твоё время ответа: ' + timeyou + ' сек')
+    bot.send_message(message.from_user.id, name + "твоё время ответа: " + str(timeyou) + " сек")
 
 bot.polling(none_stop=False, interval=0)
